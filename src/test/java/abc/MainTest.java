c874eac4e840e1e5dba519705c9e53083038c4de
Christian Wulf <chw@informatik.uni-kiel.de>
2019-04-22 20:30:42 +0200

    added MainTest

---
+++ src/test/java/abc/MainTest.java
@@ -0,0 +1,26 @@
+package abc;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import java.net.URL;
+import java.nio.file.Paths;
+
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void shouldParseArgsCorrectly() throws Exception {
+		URL oldVersionResource = MainTest.class.getResource("/teetime-1.0.jar");
+		String oldVersionJarPath = Paths.get(oldVersionResource.toURI()).toString();
+
+		URL currentVersionResource = MainTest.class.getResource("/teetime-2.0.jar");
+		String currentVersionJarPath = Paths.get(currentVersionResource.toURI()).toString();
+
+		String[] args = { "-o", oldVersionJarPath, "-c", currentVersionJarPath };
+		int numInvalidChanges = Main.mainWithoutExitCall(args);
+
+		assertThat(numInvalidChanges, is(74));
+	}
+}
